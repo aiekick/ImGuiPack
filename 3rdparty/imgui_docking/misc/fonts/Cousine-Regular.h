8fdd2ff5cfe5d305552ae87c39b07a6c78be2dae
aiekick <aiekick@funparadigm.com>
2025-08-15 11:10:30 +0200

    -

---
+++ 3rdparty/imgui_docking/misc/fonts/Cousine-Regular.h
@@ -0,0 +1,7 @@
+//Header Generated with https://github.com/aiekick/ImGuiFontStudio
+//for Language c++
+
+#pragma once
+
+extern const unsigned int CR_compressed_data_base85_size;
+extern const char CR_compressed_data_base85[];
---
+++ 3rdparty/imgui_docking/misc/fonts/Karla-Regular.h
@@ -0,0 +1,7 @@
+//Header Generated with https://github.com/aiekick/ImGuiFontStudio
+//for Language c++
+
+#pragma once
+
+extern const unsigned int KR_compressed_data_base85_size;
+extern const char KR_compressed_data_base85[];
